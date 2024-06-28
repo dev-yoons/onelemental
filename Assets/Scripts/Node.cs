5f16c487350138a9ebb191100aa80c2515de974a
jjyyuunn <jjyyuunn@yonsei.ac.kr>
2024-06-28 23:04:30 +0900

    다시 세팅..

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int CurrentWorshipers { get; set; }
+    public int WorshipersToSend { get; set; }
+    public float Speed { get; set; }
+    public bool OnAttack { get; set; }
+    
+    private Coroutine _attackCoroutine;
+
+    public GameObject worshiperObject;
+    
+    /// <summary>
+    /// 다른 노드로 공격 보내기
+    /// </summary>
+    /// <param name="destination"></param>
+    // public void SendAttack(Node destination)
+    // {
+    //     if (_attackCoroutine != null)
+    //     {
+    //         StopCoroutine(_attackCoroutine);
+    //     }
+    //     _attackCoroutine = StartCoroutine(SendWorshipers(destination));
+    // }
+
+    // private IEnumerator SendWorshipers(Node destination)
+    // {
+    //     while (WorshipersToSend > 0)
+    //     {
+    //         // 숭배자 생성 및 초기화
+    //         GameObject worshiper = Instantiate(worshiperObject, transform.position, Quaternion.identity);
+    //         Worshiper worshiperComponent = worshiper.GetComponent<Worshiper>();
+    //         worshiperComponent.Initialize(otherNode, Speed);
+    //
+    //         // 숭배자 수 감소
+    //         Worshipers--;
+    //
+    //         // 잠시 대기 후 반복
+    //         yield return new WaitForSeconds(1.0f); // 필요에 따라 시간 조정
+    //     }
+    // }
+}

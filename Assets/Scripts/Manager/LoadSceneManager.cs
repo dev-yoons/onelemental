d2e82bec962b49ab9082d706726430d1e613ba95
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2024-06-29 22:05:31 +0900

    LoadSceneManager 수정

--- Assets/Scripts/Manager/LoadSceneManager.cs
+++ Assets/Scripts/Manager/LoadSceneManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,6 +20,29 @@ public class LoadSceneManager : MonoBehaviour
 
     public void NextStage()
     {
-        SceneManager.LoadScene("Stage2"); // 수정 필요
+        string currentScene = SceneManager.GetActiveScene().name;
+        IncrementSceneNumber(currentScene);
+    }
+
+    string IncrementSceneNumber(string sceneName)
+    {
+        // 정규 표현식으로 숫자 추출
+        Match match = Regex.Match(sceneName, @"\d+");
+
+        if (match.Success)
+        {
+            // 숫자 부분이 발견되면 이를 정수로 변환하여 1 더하기
+            int number = int.Parse(match.Value);
+            number += 1;
+
+            // 새로운 씬 이름 생성 (기존 문자열에서 숫자 부분을 새로운 숫자로 교체)
+            string newSceneName = Regex.Replace(sceneName, @"\d+", number.ToString());
+            return newSceneName;
+        }
+        else
+        {
+            // 숫자 부분이 없으면 원래 문자열 반환 (또는 다른 예외 처리)
+            return sceneName;
+        }
     }
 }

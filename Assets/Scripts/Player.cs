da44ced8f726285693c2653c03bd19a2bb14ae41
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2024-06-29 14:56:29 +0900

    Stage1 Scene 중립지 추가 및 위치조정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,18 +33,18 @@ public class Player : MonoBehaviour
      
     public virtual void Defeat()
     {
-        // 기획 질문 : 내가 가지고 있던 거점 노드는 어떻게 되는 건지?
-        // 현재: 가지고 있는 노드 중립 지역으로, OwningNodes에서 제거
-        // AI 문제 해결되면 테스트 해볼게요
-        foreach (Node node in OwningNodes)
-        {
-            node.SetCurrentElemental(Elemental.Neutral);
-            node.CurrentWorshipers = 0;
-            OwningNodes.Remove(node);
-        }
+        //// 기획 질문 : 내가 가지고 있던 거점 노드는 어떻게 되는 건지?
+        //// 현재: 가지고 있는 노드 중립 지역으로, OwningNodes에서 제거
+        //// AI 문제 해결되면 테스트 해볼게요
+        //foreach (Node node in OwningNodes)
+        //{
+        //    node.SetCurrentElemental(Elemental.Neutral);
+        //    node.CurrentWorshipers = 0;
+        //    OwningNodes.Remove(node);
+        //}
 
-        MainNode.IsMainNode = false;
-        // 플레이어 화면에서 사라짐
-        Destroy(this.gameObject);
+        //MainNode.IsMainNode = false;
+        //// 플레이어 화면에서 사라짐
+        //Destroy(this.gameObject);
     }
 }

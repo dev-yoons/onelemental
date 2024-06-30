63ab7e433cb10944a8e5897928f53a3c1fbb1539
dev-yoons <dev.yoons99@gmail.com>
2024-06-30 12:21:16 +0900

    [이윤서] 드래그 중에 클릭 불가 상태가 되는 것을 방지

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -69,15 +69,18 @@ public class DrawLine : MonoBehaviour
             Node endNode = hit.collider?.GetComponent<Node>();
             if (endNode != null && endNode != _startNode && _startNode.IsConnectedToNode(endNode))
             {
-                // endNode가 플레이어의 노드인지 && startNode에 공격을 보내고 있는지 확인
-                if (!(GameManager.StageRuleManager.PlayerOwnsNode(endNode) && endNode.IsBeingAttackedBy(_startNode)))
+                if (GameManager.StageRuleManager.IsClickableNode(_startNode))
                 {
-                    GameObject lineObject = Instantiate(GameManager.PrefabManager.LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    // endNode가 플레이어의 노드인지 && startNode에 공격을 보내고 있는지 확인
+                    if (!(GameManager.StageRuleManager.PlayerOwnsNode(endNode) && endNode.IsBeingAttackedBy(_startNode)))
+                    {
+                        GameObject lineObject = Instantiate(GameManager.PrefabManager.LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
 
-                    lineObject.GetComponent<Line>().Init(_startNode, endNode); 
+                        lineObject.GetComponent<Line>().Init(_startNode, endNode);
 
-                    _startNode.SendAttack(endNode.gameObject, lineObject.GetComponent<Line>());
-                }
+                        _startNode.SendAttack(endNode.gameObject, lineObject.GetComponent<Line>());
+                    }
+                } 
             }
 
             _lineRenderer.enabled = false; 
